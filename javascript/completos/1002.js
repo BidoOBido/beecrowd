a95afa2833a6824ed8e9e0aa9e8faf542e04f29e
Mailon Bido <mailonbido@gmail.com>
2021-03-04 07:57:11 -0300

    Adição da resolução do problema 1002 em javascript

---
+++ javascript/completos/1002.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const PI = 3.14159
+
+var raio = parseFloat(lines.shift());
+var area = PI * Math.pow(raio, 2)
+
+console.log('A='+area.toFixed(4))
