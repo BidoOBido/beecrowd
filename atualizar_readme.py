b78fb467f9534d76cd7ce3f26435345b180cc123
Mailon Bido <mailonbido@yahoo.com.br>
2021-11-28 21:04:20 -0300

    Atualizado arquivo readme

--- atualizar_readme.py
+++ atualizar_readme.py
@@ -15,43 +15,67 @@ def calcular_barra(tamanho: int, total: int, completo: int) -> tuple():
 
 def formatar_progresso(estatisticas: dict) -> str:
     tentados, completos = estatisticas.values()
-    pretos, brancos = calcular_barra(20, tentados, completos)
-    percentual = round(((completos/tentados) * 100), 2)
+    pretos, brancos = calcular_barra(30, tentados, completos)
+    percentual = round(((completos / tentados) * 100), 2)
 
     return f"{completos:4} completos de {tentados:4} tentados {UNICODE_BRANCO * brancos}{UNICODE_PRETO * pretos} {percentual:.2f}%"
 
-def obter_totais(dir: str) -> int:
-    if (os.path.isdir(dir)):
-        todos_arquivos = [file[2] for file in os.walk(dir)][0]
-        return len(list(dict.fromkeys([file[0:4] for file in todos_arquivos]))) 
 
-    return 0   
+def obter_totais(dir: str) -> int:
+    if os.path.isdir(dir):
+        todos_arquivos = [file[2] for file in os.walk(dir)][0]
+        todos_arquivos = list(dict.fromkeys([file[0:4] for file in todos_arquivos]))
+        return (len(todos_arquivos), todos_arquivos)
+
+    return (0, [])
+
 
 def gera_estatisticas():
     estatisticas = {}
     exercicios = {}
+    total_geral_completos = []
+    total_geral_incompletos = []
 
     for language in os.listdir(os.curdir):
         if (os.path.isdir(language)) and (not language.startswith(".")):
-            total_completos = obter_totais(os.path.join(language, 'completos')) 
-            total_incompletos = obter_totais(os.path.join(language, 'incompletos')) 
-        
-            exercicios[language] = {"tentados": total_completos + total_incompletos, "completos": total_completos}
+            total_completos, lista_completos = obter_totais(
+                os.path.join(language, "completos")
+            )
+            total_incompletos, lista_incompletos = obter_totais(
+                os.path.join(language, "incompletos")
+            )
+
+            total_geral_completos.extend(lista_completos)
+            total_geral_incompletos.extend(lista_incompletos)
+
+            exercicios[language] = {
+                "tentados": total_completos + total_incompletos,
+                "completos": total_completos,
+            }
 
     for linguagem, valores in exercicios.items():
-        estatisticas[linguagem] = (formatar_progresso(valores))
+        estatisticas[linguagem] = formatar_progresso(valores)
 
-    return estatisticas
+    total_completo = len(list(dict.fromkeys(total_geral_completos)))
+    total_geral = total_completo + len(list(dict.fromkeys(total_geral_incompletos)))
+
+    return (estatisticas, {"total": total_geral, "completo": total_completo})
+
 
 def main():
-    with open(os.path.join(os.curdir, 'readme.md'), 'w') as f:
-        f.write(TEXTO_PADRAO)  
+    estatisticas, gerais = gera_estatisticas()
 
-        for linguagem, valor in gera_estatisticas().items():
-            f.write(f"- [{linguagem}](./{linguagem}/)\n\n")    
-            f.write('```txt') 
+    with open(os.path.join(os.curdir, "readme.md"), "w") as f:
+        f.write(TEXTO_PADRAO)
+
+        f.write(f"Total geral: `{formatar_progresso(gerais).lstrip()}`\n\n")
+
+        for linguagem, valor in estatisticas.items():
+            f.write(f"- [{linguagem}](./{linguagem}/)\n\n")
+            f.write("```txt")
             f.write(f"\n{valor}\n")
-            f.write('```\n\n')
+            f.write("```\n\n")
+
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -4,51 +4,53 @@ Propostas de resoluções dos exercícios do site [beecrowd](https://www.beecrow
 
 Atualmente existem resoluções para exercícios nas seguintes linguagens (divididos entre **completos** e **incompletos**):
 
+Total geral: `128 completos de  139 tentados ███████████████████████████░░░ 92.09%`
+
 - [sql](./sql/)
 
 ```txt
-  34 completos de   34 tentados ████████████████████ 100.00%
+  34 completos de   34 tentados ██████████████████████████████ 100.00%
 ```
 
 - [c](./c/)
 
 ```txt
-  10 completos de   10 tentados ████████████████████ 100.00%
+  10 completos de   10 tentados ██████████████████████████████ 100.00%
 ```
 
 - [go](./go/)
 
 ```txt
-   2 completos de    3 tentados █████████████░░░░░░░ 66.67%
+   2 completos de    3 tentados ████████████████████░░░░░░░░░░ 66.67%
 ```
 
 - [python](./python/)
 
 ```txt
-  70 completos de   79 tentados █████████████████░░░ 88.61%
+  70 completos de   79 tentados ██████████████████████████░░░░ 88.61%
 ```
 
 - [java](./java/)
 
 ```txt
-  21 completos de   22 tentados ███████████████████░ 95.45%
+  21 completos de   22 tentados ████████████████████████████░░ 95.45%
 ```
 
 - [c++](./c++/)
 
 ```txt
-   3 completos de    3 tentados ████████████████████ 100.00%
+   3 completos de    3 tentados ██████████████████████████████ 100.00%
 ```
 
 - [javascript](./javascript/)
 
 ```txt
-   7 completos de    7 tentados ████████████████████ 100.00%
+   7 completos de    7 tentados ██████████████████████████████ 100.00%
 ```
 
 - [ruby](./ruby/)
 
 ```txt
-  11 completos de   11 tentados ████████████████████ 100.00%
+  11 completos de   11 tentados █████████████████████████████░ 100.00%
 ```
 
